f93321caa1e46b7cf379b786a48ee50d7e071e65
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2025-10-20 17:09:01 +0000

    fix posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.tailwind.css';
+	import { posthog } from 'posthog-js';
 	import favicon from '$lib/assets/favicon.svg';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
@@ -16,6 +17,11 @@
 
 		return () => data.subscription.unsubscribe();
 	});
+	posthog.init('phc_CbHOw79fze405aeNYJKiCPEb8jLqujU2zKKWWRHdpR9', {
+        api_host: 'https://us.i.posthog.com',
+        defaults: '2025-05-24',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    });
 </script>
 
 <svelte:head>
